e610eba34e745487e455cac331b3341cd799b2f1
عبد الرحمان <71411025+Moumin01@users.noreply.github.com>
2023-03-09 21:10:57 +0100

    Update hadith.php

--- database/hadith.php
+++ database/hadith.php
@@ -6,7 +6,7 @@ class hadith{
     public function getSpecificHadith($collection, $number){
         try{
             $db = new database();
-            $stmt = $db->conn->prepare("SELECT * FROM haditharabic WHERE collection= :collection AND hadithNumber= :number");
+            $stmt = $db->conn->prepare("SELECT * FROM hadith WHERE collection= :collection AND hadithNumber= :number");
             $stmt->execute([
                 'collection'=>$collection,
                 'number'=>$number
@@ -14,14 +14,14 @@ class hadith{
             $data = ['statusCode'=>200,'statusMessage'=>'Ok','data'=> $stmt->fetchAll(PDO::FETCH_ASSOC), 'timestamp'=>time()];
             return json_encode($data,JSON_UNESCAPED_UNICODE);
         }catch (Exception $e){
-            echo $e;
+            print_r($e);
         }
     }
 
     public function getAllHadith($collection, $number){
         try{
             $db = new database();
-            $stmt = $db->conn->prepare("SELECT * FROM haditharabic WHERE collection= :collection AND chapter_number= :number ORDER BY hadithNumber");
+            $stmt = $db->conn->prepare("SELECT * FROM hadith WHERE collection= :collection AND chapter_number= :number ORDER BY hadithNumber");
             $stmt->execute([
                 'collection'=>$collection,
                 'number'=>$number
@@ -29,53 +29,53 @@ class hadith{
             $data = ['statusCode'=>200,'statusMessage'=>'Ok','data'=> $stmt->fetchAll(PDO::FETCH_ASSOC), 'timestamp'=>time()];
             return json_encode($data,JSON_UNESCAPED_UNICODE);
         }catch (Exception $e){
-            echo $e;
+            print_r($e);
         }
     }
 
     public function getRandomHadith(){
         try{
             $db = new database();
-            $stmt = $db->conn->prepare("SELECT * FROM haditharabic WHERE Text REGEXP '[A-Za-z0-9]' ORDER BY RAND() LIMIT 1");
+            $stmt = $db->conn->prepare("SELECT * FROM hadith ORDER BY RAND() LIMIT 1");
             $stmt->execute();
             $data = ['statusCode'=>200,'statusMessage'=>'Ok','data'=> $stmt->fetchAll(PDO::FETCH_ASSOC), 'timestamp'=>time()];
             return json_encode($data,JSON_UNESCAPED_UNICODE);
         }catch (Exception $e){
-            echo $e;
+            print_r($e);
         }
     }
     public function getHadithChapters($collection){
         try{
             $db = new database();
-            $stmt = $db->conn->prepare("SELECT DISTINCT chapter,chapter_number FROM haditharabic WHERE collection= :collection ORDER BY chapter_number");
+            $stmt = $db->conn->prepare("SELECT DISTINCT chapter,chapter_number FROM hadith WHERE collection= :collection ORDER BY chapter_number");
             $stmt->execute(['collection'=>$collection]);
             $data = ['statusCode'=>200,'statusMessage'=>'Ok','data'=> $stmt->fetchAll(PDO::FETCH_ASSOC), 'timestamp'=>time()];
             return json_encode($data,JSON_UNESCAPED_UNICODE);
         }catch (Exception $e){
-            echo $e;
+            print_r($e);
         }
     }
 
     public function searchHadith($search){
         try{
             $db = new database();
-            $stmt = $db->conn->prepare('SELECT * FROM haditharabic WHERE text LIKE "%?%" ');
+            $stmt = $db->conn->prepare('SELECT * FROM hadith WHERE text AND Text_en AND transmiter LIKE "%?%" ');
             $stmt->execute([$search]);
             $data = ['statusCode'=>200,'statusMessage'=>'Ok','data'=> $stmt->fetchAll(PDO::FETCH_ASSOC), 'timestamp'=>time()];
             return json_encode($data,JSON_UNESCAPED_UNICODE);
         }catch (Exception $e){
-            echo $e;
+            print_r($e);
         }
     }
     public function getAllBooks(){
         try{
             $db = new database();
-            $stmt = $db->conn->prepare('SELECT DISTINCT collection FROM haditharabic ORDER BY collection ASC');
+            $stmt = $db->conn->prepare('SELECT DISTINCT collection FROM hadith ORDER BY collection ASC');
             $stmt->execute();
             $data = ['statusCode'=>200,'statusMessage'=>'Ok','data'=> $stmt->fetchAll(PDO::FETCH_ASSOC), 'timestamp'=>time()];
             return json_encode($data,JSON_UNESCAPED_UNICODE);
         }catch (Exception $e){
-            echo $e;
+            print_r($e);
         }
     }
 }
