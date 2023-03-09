ce7ef623d982e3329794a877b7cfa1b2839a99e7
عبد الرحمان <71411025+Moumin01@users.noreply.github.com>
2023-03-09 21:10:09 +0100

    Update db.php

--- database/db.php
+++ database/db.php
@@ -10,7 +10,7 @@ private $db = "";
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
+            print_r("Database Failed");
         }
     }
 }
